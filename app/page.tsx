49c450460d4448144c65586a86cebf89271e67de
nipun221 <2oics072@gbu.ac.in>
2023-07-27 22:05:09 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { ProjectInterface } from "@/common.types";
+import Categories from "@/components/Categories";
+import LoadMore from "@/components/LoadMore";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
+import { type } from "os";
 
 type ProjectSearch = {
     projectSearch: {
@@ -14,15 +17,28 @@ type ProjectSearch = {
     }
 }
 
-const Home = async () => {
-    const data = await fetchAllProjects() as ProjectSearch
+type SearchParams = {
+    category?: string;
+    endcursor?: string;
+}
+
+type Props = {
+    searchParams: SearchParams;
+}
+
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+export const revalidate = 0;
+
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+    const data = await fetchAllProjects(category, endcursor) as ProjectSearch
 
     const projectsToDisplay = data?.projectSearch?.edges || [];
 
     if(projectsToDisplay.length === 0) {
         return (
             <section className="flexStart flex-col paddings">
-                Categories
+                <Categories/>
 
                 <p className="no-result-text text-center">
                     No projects found, go and create first.
@@ -31,9 +47,11 @@ const Home = async () => {
         )
     }
 
+    const pagination = data?.projectSearch?.pageInfo;
+
     return (
         <section className="flex-start flex-col paddings mb-16">
-            <h1>Categories</h1>
+            <Categories/>
 
             <section className="projects-grid">
                 {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
@@ -49,7 +67,12 @@ const Home = async () => {
                 ))}
             </section>
 
-            <h1>LoadMore</h1>
+            <LoadMore
+                startCursor={pagination.startCursor}
+                endCursor={pagination.endCursor}
+                hasPreviousPage={pagination.hasPreviousPage}
+                hasNextPage={pagination.hasNextPage}
+            />
         </section>
     );
 };
